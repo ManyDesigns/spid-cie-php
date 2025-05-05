8c4a1412cee4cc6d2da0bf041e5076a2fd2ce87f
MarcoMany <marco.giordano@manydesigns.com>
2025-05-05 11:33:14 +0200

    fix: prevent warning if not existing

--- setup/simplesamlphp/saml2/src/SAML2/XML/md/EntityDescriptor.php
+++ setup/simplesamlphp/saml2/src/SAML2/XML/md/EntityDescriptor.php
@@ -507,12 +507,12 @@ class EntityDescriptor extends SignedElementHelper
         }
 
         // set spid namespace if exists
-        if ($this->ns['spid'] !== null) {
+        if (isset($this->ns['spid'])) {
             $e->setAttribute('xmlns:spid', $this->ns['spid']);
         }
 
         // set cie namespace if exists
-        if ($this->ns['cie'] !== null) {
+        if (isset($this->ns['cie'])) {
             $e->setAttribute('xmlns:cie', $this->ns['cie']);
         }
 
